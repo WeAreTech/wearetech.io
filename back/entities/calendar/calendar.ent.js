2fcd8daca8552ae047a1edbc5e4c710dc5f4938d
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-27 15:33:17 +0300

    scaffold the calendar entity

---
+++ back/entities/calendar/calendar.ent.js
@@ -0,0 +1,25 @@
+/**
+ * @fileOverview The Calendar Entity.
+ */
+
+// var __ = require('lodash');
+// var BPromise = require('bluebird');
+// var appError = require('nodeon-error');
+var EntityBase = require('nodeon-base').EntityBase;
+
+// var log = require('logg').getLogger('app.ent.Calendar');
+
+var CalendarModel = require('../../models/calendar.model');
+var calendarModel = CalendarModel.getInstance();
+
+/**
+ * The Calendar entity.
+ *
+ * @param {Object=} optUdo Optionally define the current handling Calendar.
+ * @constructor
+ * @extends {app.EntityBase}
+ */
+var Calendar = module.exports = EntityBase.extendSingleton(function() {
+  this.setModel(calendarModel.Model);
+});
+
