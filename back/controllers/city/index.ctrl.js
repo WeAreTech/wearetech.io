a15f7dfe45e115bdc400c887cadd4af1c93edc79
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-27 15:46:16 +0300

    scaffold the city entity and city controller

---
+++ back/controllers/city/index.ctrl.js
@@ -0,0 +1,26 @@
+/**
+ * @fileOverview GET / City page.
+ */
+// var log = require('logg').getLogger('app.ctrl.city.Index');
+
+var ControllerBase = require('nodeon-base').ControllerBase;
+
+/**
+ * The City home page.
+ *
+ * @contructor
+ * @extends {app.ControllerBase}
+ */
+var Home = module.exports = ControllerBase.extendSingleton(function(){
+  this.use.push(this._useIndex.bind(this));
+});
+
+/**
+ * The index page.
+ *
+ * @param {Object} req The request Object.
+ * @param {Object} res The response Object.
+ */
+Home.prototype._useIndex = function(req, res) {
+  res.render('city/index');
+};
---
+++ back/entities/city.ent.js
@@ -0,0 +1,24 @@
+/**
+ * @fileOverview The City Entity.
+ */
+
+// var __ = require('lodash');
+// var BPromise = require('bluebird');
+// var appError = require('nodeon-error');
+var EntityBase = require('nodeon-base').EntityBase;
+
+// var log = require('logg').getLogger('app.ent.City');
+
+var CityModel = require('../../models/City.model');
+var cityModel = CityModel.getInstance();
+
+/**
+ * The City entity.
+ *
+ * @constructor
+ * @extends {app.EntityBase}
+ */
+var City = module.exports = EntityBase.extendSingleton(function() {
+  this.setModel(cityModel.Model);
+});
+
