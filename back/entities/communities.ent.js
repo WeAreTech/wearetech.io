79a5cdcf5553b3dabff7aac00506d06d46fc1f2a
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-27 15:41:24 +0300

    scaffold community entity

---
+++ back/entities/communities.ent.js
@@ -0,0 +1,24 @@
+/**
+ * @fileOverview The Community Entity.
+ */
+
+// var __ = require('lodash');
+// var BPromise = require('bluebird');
+// var appError = require('nodeon-error');
+var EntityBase = require('nodeon-base').EntityBase;
+
+// var log = require('logg').getLogger('app.ent.Community');
+
+var CommunityModel = require('../../models/Community.model');
+var communityModel = CommunityModel.getInstance();
+
+/**
+ * The Community entity.
+ *
+ * @constructor
+ * @extends {app.EntityBase}
+ */
+var Community = module.exports = EntityBase.extendSingleton(function() {
+  this.setModel(communityModel.Model);
+});
+
