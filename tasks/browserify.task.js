1cef3d03d70ad974c757c4064844b410534960e8
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-28 11:12:20 +0300

    create the browserify task

---
+++ tasks/browserify.task.js
@@ -0,0 +1,14 @@
+/**
+ * @fileOverview The Browserify task operation.
+ */
+module.exports = function(grunt) {
+  grunt.config('browserify', {
+    dist: {
+      files: {
+        'front/static/scripts/city.src.js': ['front/styles-city/app.js'],
+      },
+    }
+  });
+
+};
+
