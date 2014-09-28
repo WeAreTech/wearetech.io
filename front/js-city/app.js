bdea477b8e340cc3407cbad0662b340472cddefa
Thanasis Polychronakis <thanpolas@gmail.com>
2014-09-28 11:07:27 +0300

    move over frontapp codebase from skgtech

---
+++ front/js-city/app.js
@@ -0,0 +1,16 @@
+/*
+ * SKG Tech
+ * Thessaloniki Tech Events and Meetups
+ * https://github.com/skgtech/skgtech.github.io
+ *
+ * Copyright (c) 2014 SKG Tech Contributors.
+ * Licensed under the MIT license.
+ */
+
+/**
+ * @fileOverview City Frontend application bootstrap file
+ */
+var Front = require('./frontpage');
+
+var front = new Front();
+front.init();
