e6bb1c85995e09662aa7a447e2f0e509c741d1b1
Kostas <konmpar@gmail.com>
2014-10-05 13:54:23 +0300

    registration form now checks for city availability first

--- front/js-main/register.js
+++ front/js-main/register.js
@@ -1,4 +1,3 @@
-
 var Register = module.exports = function() {};
 
 /**
@@ -6,4 +5,48 @@ var Register = module.exports = function() {};
  *
  */
 Register.prototype.init = function() {
+  this.$cityControl = $('#city');
+
+  var input = document.getElementById('city');
+  var options = {
+    types: ['(cities)'],
+    componentRestrictions: {country: 'gr'}
+  };
+
+  autocomplete = new google.maps.places.Autocomplete(input, options);
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (place.geometry) {
+      map.panTo(place.geometry.location);
+      map.setZoom(15);
+
+      var marker = new google.maps.Marker({
+        position: place.geometry.location,
+        animation: google.maps.Animation.DROP
+      });
+
+      marker.setMap(map);
+
+      var url = 'http://localhost:3000/available?place_id=' + place.place_id
+      $.get(url, function(response){
+        if(response.city && response.city != null) {
+          $('.user-registration-form').removeClass('hidden');
+          $('.form-title').text($('.form-title').text() + place.formatted_address)
+        }
+      });
+    }
+
+  });
+
+  var myOptions = {
+    center: new google.maps.LatLng(54.8, -4.6),
+    zoom: 5,
+    mapTypeControl: false,
+    panControl: false,
+    zoomControl: false,
+    streetViewControl: false
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
 };
