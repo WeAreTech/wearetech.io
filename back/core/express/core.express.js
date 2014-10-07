64d02c0c52281f7c2cf4147b44bcaeb5806f7ba0
Thanasis Polychronakis <thanpolas@gmail.com>
2014-10-07 19:44:15 +0300

    try the wildcard on vhosts as an addition vs replacement

--- back/core/express/core.express.js
+++ back/core/express/core.express.js
@@ -90,6 +90,7 @@ ExpressApp.prototype.init = BPromise.method(function(opts) {
       this.app.use(vhost(config.hostname.city, appApi));
       // SKIN it for now..
       this.app.use(vhost('skgtech.io', appApi));
+      this.app.use(vhost('*.skgtech.io', appApi));
       this.app.use(vhost('athtech.org', appApi));
     }
 
