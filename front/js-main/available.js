1be5abd6988556e45ee166dd710d1a64d8a91975
Kostas <konmpar@gmail.com>
2014-10-10 21:19:17 +0300

    available.js refactored

--- front/js-main/available.js
+++ front/js-main/available.js
@@ -1,62 +1,54 @@
 /**
  * @fileOverview City Registration app
  */
-var Available = module.exports = function() {};
+var Available = module.exports = function () {};
 
 /**
  * Initialize the Available view.
  */
-Available.prototype.init = function() {
-  this.$cityControl = $('#city');
+Available.prototype.init = function () {
+  this._initAutocomplete();
+};
 
-  var input = document.getElementById('city');
-  var options = {
+/**
+ * Initialize Google's Autocomplete library
+ *
+ * @todo get a list of Country's Geo values to populate the Country's select box
+ * like this https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete-hotelsearch
+ *
+ * @see https://developers.google.com/maps/documentation/javascript/places-autocomplete
+ */
+Available.prototype._initAutocomplete = function () {
+
+  var cityInput = document.getElementById('city');
+
+  autocomplete = new google.maps.places.Autocomplete(cityInput, {
     types: ['(cities)'],
     componentRestrictions: {country: 'gr'}
-  };
+  });
 
-  autocomplete = new google.maps.places.Autocomplete(input, options);
-
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+  google.maps.event.addListener(autocomplete, 'place_changed', function () {
     var place = autocomplete.getPlace();
     if (place.geometry) {
       map.panTo(place.geometry.location);
       map.setZoom(15);
 
       var marker = new google.maps.Marker({
-        position: place.geometry.location,
-        animation: google.maps.Animation.DROP
-      });
+                                            position: place.geometry.location,
+                                            animation: google.maps.Animation.DROP
+                                          });
 
       marker.setMap(map);
-
-      var url = 'http://localhost:3000/available?place_id=' + place.place_id
-      $.get(url, function(response){
-        if(response.city && response.city != null) {
-          $('.user-registration-form').removeClass('hidden');
-          $('.form-title').html($('.form-title').text() + '<br/>' + place.formatted_address);
-
-
-          $('<input>').attr({
-            type: 'hidden',
-            name: 'cityName',
-            value: place.formatted_address
-          }).appendTo($('.user-registration-form form'));
-
-        }
-      });
     }
 
   });
 
-  var myOptions = {
+  map = new google.maps.Map(document.getElementById('map-canvas'), myOptions = {
     center: new google.maps.LatLng(54.8, -4.6),
     zoom: 5,
     mapTypeControl: false,
     panControl: false,
     zoomControl: false,
     streetViewControl: false
-  };
-
-  map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
+  });
 };
