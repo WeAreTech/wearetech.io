613f7a2b9a221be6aad3211aa8adc053d4ed934c
Thanasis Polychronakis <thanpolas@gmail.com>
2014-10-12 14:30:30 +0300

    scaffold the Together entity

---
+++ back/entities/together.ent.js
@@ -0,0 +1,25 @@
+/**
+ * @fileOverview The Together Entity.
+ */
+
+// var __ = require('lodash');
+// var BPromise = require('bluebird');
+// var appError = require('nodeon-error');
+var EntityBase = require('nodeon-base').EntityBase;
+
+// var log = require('logg').getLogger('app.ent.Together');
+
+var TogetherModel = require('../models/Together.model');
+var togetherModel = TogetherModel.getInstance();
+
+/**
+ * The Together entity.
+ *
+ * @constructor
+ * @extends {app.EntityBase}
+ */
+var Together = module.exports = EntityBase.extendSingleton(function() {
+  this.setModel(togetherModel.Model);
+});
+
+Together.STUB = 1;
