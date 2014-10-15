cb7aab0d1fe05adc02ab1762496b1c68271cd69a
Thanasis Polychronakis <thanpolas@gmail.com>
2014-10-15 12:26:26 +0300

    tweak / fix OG tags on together page

--- back/controllers/city/together.ctrl.js
+++ back/controllers/city/together.ctrl.js
@@ -57,7 +57,7 @@ Home.prototype._populateOgTags = function(req, res, next) {
     return;
   }
   var origUrl = req.city.og.url;
-  res.locals.city.og.url += '/together';
+  res.locals.city.og.url = 'http://' + origUrl + '/together';
   res.locals.city.og.image = 'http://' + origUrl + '/img/we-are-together-logo.png';
   res.locals.city.og.description = res.locals.together.tagline;
   next();
