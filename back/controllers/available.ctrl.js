d53b094bede74898349f38659c011aa9f6a327c7
Kostas <konmpar@gmail.com>
2014-10-18 21:51:39 +0300

    available post redirects to user registration

--- back/controllers/available.ctrl.js
+++ back/controllers/available.ctrl.js
@@ -1,7 +1,6 @@
 /**
  * @fileOverview GET /available page.
  */
-
 var ControllerBase = require('nodeon-base').ControllerBase;
 
 /**
@@ -10,22 +9,24 @@ var ControllerBase = require('nodeon-base').ControllerBase;
  * @contructor
  * @extends {app.ControllerBase}
  */
-var Available = module.exports = ControllerBase.extendSingleton(function(){
+var Available = module.exports = ControllerBase.extendSingleton(function () {
   this.use.push(this._registerNewCity.bind(this));
 });
 
-Available.prototype._registerNewCity = function(req, res) {
+Available.prototype._registerNewCity = function (req, res) {
   if (req.method === 'GET') {
     this._getAvailableView(req, res);
     return;
   }
 
-  res.render('city/available');
+  /**
+   * @todo: Parse body, register city and pass cityName to req
+   */
+
+  res.redirect('/register');
 
 };
 
-Available.prototype._getAvailableView = function(req, res) {
-
+Available.prototype._getAvailableView = function (req, res) {
   res.render('city/available');
-
 };
