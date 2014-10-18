330d6b264f4919f3515b3b6ef1afb7927b27de0a
Kostas <konmpar@gmail.com>
2014-10-18 21:03:43 +0300

    population of hidden form, form validation, refactoring

--- front/js-main/available.js
+++ front/js-main/available.js
@@ -4,17 +4,34 @@
 var Available = module.exports = function () {
   this.$mapContainer = null;
   this.$searchCityForm = null;
+  this.$searchCityInput = null;
   this.$autocompleteObject = null;
+
+  this.$cityCanonicalNameField = null;
+  this.$cityCountryCodeField = null;
+  this.$cityLatField = null;
+  this.$cityLngField = null;
 };
 
 /**
  * Initialize the Available view.
  */
 Available.prototype.init = function () {
-  this.$mapContainer = null;
   this.$searchCityForm = $('.search-city-form');
+  this.$searchCityInput = $('#city');
 
-  this.$searchCityForm.on('submit', function () { return false; });
+  this.$cityCanonicalNameField = $('input[name=cityCanonicalName]');
+  this.$cityCountryCodeField = $('input[name=cityCountryCode]');
+  this.$cityLatField = $('input[name=cityLat]');
+  this.$cityLngField = $('input[name=cityLng]');
+
+  this.$searchCityForm.on('keydown', function (event) {
+    if (event.keyCode === 13) {
+      return false;
+    }
+  });
+
+  this.$searchCityForm.on('submit', this._submitForm.bind(this));
 
   this._initAutocomplete();
   this._initMap();
@@ -30,13 +47,11 @@ Available.prototype.init = function () {
  */
 Available.prototype._initAutocomplete = function () {
 
-  var cityInput = document.getElementById('city');
-
   var options = {
     types: ['(cities)'],
     componentRestrictions: {country: 'gr'}
   };
-  this.$autocompleteObject = new google.maps.places.Autocomplete(cityInput, options);
+  this.$autocompleteObject = new google.maps.places.Autocomplete(this.$searchCityInput[0], options); /* Pass native DOM element */
 
   google.maps.event.addListener(this.$autocompleteObject, 'place_changed', this._changeMapLocation.bind(this));
 
@@ -66,5 +81,45 @@ Available.prototype._changeMapLocation = function () {
     });
 
     marker.setMap(this.$mapContainer);
+
+    this._populateHiddenForm(place);
+  }
+};
+
+/**
+ * Populate hidden inputs after valid city selection
+ * @param place Google PlaceResult https://developers.google.com/maps/documentation/javascript/reference#PlaceResult
+ * @private
+ */
+Available.prototype._populateHiddenForm = function (place) {
+  
+  if (place.address_components && place.address_components[3] != null) {
+    this.$cityCanonicalNameField.val(place.address_components[3].long_name);
+  }
+
+  if (place.address_components && place.address_components[6] != null) {
+    this.$cityCountryCodeField.val(place.address_components[3].short_name);
+  }
+
+  if (place.geometry && place.geometry.location) {
+    this.$cityLatField.val(place.geometry.location.B);
+    this.$cityLngField.val(place.geometry.location.k);
+  }
+
+};
+
+Available.prototype._submitForm = function () {
+
+  /**
+   * Validate hidden inputs
+   */
+  if(
+    !this.$cityCanonicalNameField.val() ||
+    !this.$cityCountryCodeField.val() ||
+    !this.$cityLatField.val() ||
+    !this.$cityLngField.val()
+  ) {
+    this.$searchCityInput.parent().parent().addClass('has-error');
+    return false;
   }
 };
