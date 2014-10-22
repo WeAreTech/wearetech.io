197d777738e23cd9b3c9366c675c8f0cb16ed6bf
Kostas <konmpar@gmail.com>
2014-10-22 22:47:32 +0300

    entities added to controller

--- back/controllers/available.ctrl.js
+++ back/controllers/available.ctrl.js
@@ -1,7 +1,11 @@
 /**
  * @fileOverview GET /available page.
  */
+var validator = require('validator');
+var log = require('logg').getLogger('app.ctrl.Register');
 var ControllerBase = require('nodeon-base').ControllerBase;
+
+var CityEntity = require('../entities/city.ent');
 
 /**
  * Check for city availability
@@ -19,11 +23,65 @@ Available.prototype._registerNewCity = function (req, res) {
     return;
   }
 
-  /**
-   * @todo: Parse body, register city and pass cityName to req
-   */
+  var userCity = CityEntity.getInstance();
 
-  res.redirect('/register');
+  /**
+   * @check if city is available
+   */
+  var maxDistance = 10;
+  var geo = [22.944419100000005, 40.6400629];
+
+  var lonLat = {$geometry: {type: 'Point', coordinates: geo}};
+
+  userCity.read({
+    geo: {
+      $near: lonLat,
+      $maxDistance: maxDistance
+    }
+  }).bind(this)
+    .then(function (result) {
+      if (!result) {
+
+        /**
+         * Create new
+         */
+
+        // perform basic sanitizations, validation happens in model.
+        var params = {
+          fullName: validator.toWebstring(req.body.name, 32),
+          canonicalName: validator.toWebstring(req.body.cityCanonicalName, 64),
+          countryCode: validator.toWebstring(req.body.cityCountryCode, 2),
+          lat: req.body.cityLat,
+          lng: req.body.cityLng,
+        };
+
+        log.info('_registerNewCity() :: New city register:', params.email);
+
+        userCity.register(params)
+          .then(this._newUser.bind(this, req, res))
+          .catch(function (err) {
+            log.warn('_useRegister() :: New user fail:', err.message);
+            res.status(400).render('user/register', {
+              error: true,
+              errorMessage: err.message,
+            });
+            return;
+          });
+
+        req.flash('cityCanonicalName', req.body.cityCanonicalName);
+
+        res.redirect('/register');
+
+      } else {
+        /**
+         * We have an overlap
+         */
+        res.render('city/available', {
+          available: false,
+          cityId: result
+        });
+      }
+    });
 
 };
 
